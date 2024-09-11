0182a0d80f7f6aafffdc33d2aa4240b1c255fc26
yanabilous <yanabilous05@gmail.com>
2024-09-11 19:13:43 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import BackpackConfigurator from "./components/BackpackConfigurator";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hjdfbhgjd</h1>
+      <BackpackConfigurator/>
+    </div>
+  );
+}
+
+export default App;
