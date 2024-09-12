396662d5c08c7ac2a807124c3ffbaa76e3ccf4fb
yanabilous <yanabilous05@gmail.com>
2024-09-13 01:43:01 +0300

    add

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import ARComponent from "./components/ARComponent";
 function App() {
   return (
     <div className="App">
-      {/*<BackpackConfigurator/>*/}
-
       <Routes>
         <Route path="/" element={<BackpackConfigurator/>}/>
         <Route path="/backpack" element={<ARComponent/>}/>
       </Routes>
-
     </div>
   );
 }
--- src/components/ARComponent.jsx
+++ src/components/ARComponent.jsx
@@ -1,19 +1,19 @@
 const ARComponent = () => {
   return (
-    <a-scene
-      vr-mode-ui="enabled: true"
-      embedded
-      arjs="sourceType: webcam; trackingMethod: best; debugUIEnabled: false;">
-      <a-light type="directional" position="1 1 0" intensity="1"></a-light>
-      {/*<a-light type="ambient" color="#fff"></a-light>*/}
-      <a-gltf-model src="url(/backpack.glb)"
-                    position="0 1 -3"
-                    rotation="0 0 0"
-                    scale="4 4 4"
-                    animation="property: rotation; to: 0 360 0; dur: 10000; loop: true"
-      >
-      </a-gltf-model>
-    </a-scene>
+      <a-scene
+        vr-mode-ui="enabled: true"
+        embedded
+        arjs="sourceType: webcam; trackingMethod: best; debugUIEnabled: false;">
+        <a-light type="directional" position="1 1 0" intensity="1"></a-light>
+        {/*<a-light type="ambient" color="#fff"></a-light>*/}
+        <a-gltf-model src="url(/backpack.glb)"
+                      position="0 1 -3"
+                      rotation="0 0 0"
+                      scale="4 2 4"
+                      animation="property: rotation; to: 0 360 0; dur: 10000; loop: true"
+        >
+        </a-gltf-model>
+      </a-scene>
   );
 };
 
